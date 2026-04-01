bc93b2217b71efd718a0481f50efc80d92d30918
James <bucheckerr@gmail.com>
2026-04-01 18:11:14 -0400

    Add custom through bore encoder.

---
+++ src/org/parts3492/partslib/hardware/PARTsThroughBoreEncoder.java
@@ -0,0 +1,43 @@
+package org.parts3492.partslib.hardware;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycle;
+
+public class PARTsThroughBoreEncoder {
+  private final DutyCycle duty;
+  private final double offsetDeg;
+
+  /**
+   * Create a new Through Bore Encoder.
+   * 
+   * @param dioPort   THe Digital IO port of the encoder.
+   * @param offsetDeg The starting offset of the encoder in degrees.
+   */
+  public PARTsThroughBoreEncoder(int dioPort, double offsetDeg) {
+    this.duty = new DutyCycle(new DigitalInput(dioPort));
+    this.offsetDeg = offsetDeg;
+  }
+
+  /** Raw absolute angle in degrees [0, 360). */
+  public double getAbsoluteAngleDeg() {
+    // Fraction [0, 1). This is the PWM duty cycle.
+    double d = duty.getOutput();
+
+    // Basic mapping; see note below about min/max duty range.
+    double angle = d * 360.0;
+
+    angle %= 360.0;
+    if (angle < 0)
+      angle += 360.0;
+    return angle;
+  }
+
+  /** Absolute angle with your mechanical zero applied, degrees [0, 360). */
+  public double getAngleDeg() {
+    double angle = getAbsoluteAngleDeg() - offsetDeg;
+    angle %= 360.0;
+    if (angle < 0)
+      angle += 360.0;
+    return angle;
+  }
+}
